0d257e4bacc5ab4a0156dec7a76fd5199da77829
MastersuppVt <animastercompany@gmail.com>
2021-09-28 10:19:17 -0300

    java3

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
-var nombre = prompt("ingrese su nombre");
-var edad = Number(prompt("ingrese su edad"));
-var contraseña = prompt("ingrese su contraseña");
-while(edad < 18){
-    var edad = Number(prompt("ingrese su edad"));
-}
-alert("bienvenido");
 
-setTimeout(function(){
+setTimeout(function () {
     document.getElementById("cargadoFull").classList.add("cargadoFull2");
-    console.log("hola")},5000);
-window.addEventListener("load", function(){
-         document.getElementById("loader").classList.toggle("loader2")
-    }) 
-   
-ScrollReveal().reveal('.logo',{ delay:500 });
-ScrollReveal().reveal('.secUno',{ delay:500 });
-ScrollReveal().reveal('.secDos',{ delay:500 });
-ScrollReveal().reveal('.secTres',{ delay:500 });
-ScrollReveal().reveal('.secCuatro',{ delay:500 });
-ScrollReveal().reveal('.secCinco',{ delay:500 });
-ScrollReveal().reveal('.secSeis',{ delay:500 });
-ScrollReveal().reveal('.secSiete',{ delay:500 });
-ScrollReveal().reveal('.secOcho',{ delay:500 });
+    console.log("hola")
+}, 5000);
+window.addEventListener("load", function () {
+    document.getElementById("loader").classList.toggle("loader2")
+})
+const form = document.getElementById("formDonate")
+form.addEventListener("submit", function (event) {
+    let datosForm = new FormData(form)
+    let monto =datosForm.get("monto")
+    
+    const total = monto + 300
+    alert("su monto a pagar sera"+ " " +total)
+    event.preventDefault()
+
+})
+
+
+ScrollReveal().reveal('.logo', { delay: 500 });
+ScrollReveal().reveal('.secUno', { delay: 500 });
+ScrollReveal().reveal('.secDos', { delay: 500 });
+ScrollReveal().reveal('.secTres', { delay: 500 });
+ScrollReveal().reveal('.secCuatro', { delay: 500 });
+ScrollReveal().reveal('.secCinco', { delay: 500 });
+ScrollReveal().reveal('.secSeis', { delay: 500 });
+ScrollReveal().reveal('.secSiete', { delay: 500 });
+ScrollReveal().reveal('.secOcho', { delay: 500 });
