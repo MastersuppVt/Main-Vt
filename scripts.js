dd7e2ae41e3d914c71056dc71f0465a012f209ef
MastersuppVt <88691741+MastersuppVt@users.noreply.github.com>
2021-12-02 09:10:42 -0300

    entrega

--- scripts.js
+++ scripts.js
@@ -1,48 +1,72 @@
 setTimeout(function () {
     document.getElementById("cargadoFull").classList.add("cargadoFull2");
+    console.log("hola")
     console.log("hola");
 }, 5000);
 window.addEventListener("load", function () {
+    document.getElementById("loader").classList.toggle("loader2")
     document.getElementById("loader").classList.toggle("loader2");
 })
 $(document).ready(function () {
-    $.getJSON(url, function (data) {
-        var dailyIndicators = data;
-        $(".val").text("El valor actual del Dolar es $" + data.dolar.valor);
-    }).fail(function () {
-        console.log('Error al consumir la API!');
-    });
+    const url = "https://mindicador.cl/api"
+    const url = "https://mindicador.cl/api";
+    let ultimosDonadores = ["Pedro", "Juan", "Diego"]
+    console.log(ultimosDonadores)
+    const form = document.getElementById("formDonate")
+    const lista = document.getElementById("donadores")
+    const li = document.createElement("li")
+    localStorage.setItem("nombres", JSON.stringify(ultimosDonadores))
     console.log(ultimosDonadores);
     const form = document.getElementById("formDonate");
+    const lista = document.getElementById("donadores");
+    const li = document.createElement("li");
+    localStorage.setItem("nombres", JSON.stringify(ultimosDonadores));
     setInterval(() => {
-        $.getJSON(url, function (data) {
+        $.getJSON(url, function(data) {
             var dailyIndicators = data;
+            $(".val").text("El valor actual del Dolar es $" + data.dolar.valor)
+            console.log(data.dolar.valor)
             $(".val").text("El valor actual del Dolar es $" + data.dolar.valor);
-        }).fail(function () {
+        }).fail(function() {
             console.log('Error al consumir la API!');
         });
     }, 30000);
 
     form.addEventListener("submit", function (event) {
+        let confirmar = Number(prompt("confirma el monto"))
+        datosForm = new FormData(form)
+        let monto = Number(datosForm.get("monto"))
         let confirmar = Number(prompt("confirma el monto"));
         datosForm = new FormData(form);
         let monto = Number(datosForm.get("monto"));
         if (monto === confirmar) {
+            const total = confirmar + 300
+            alert("su monto a pagar sera" + " " + total)
+            ultimosDonadores.unshift(datosForm.get("nombre"))
+            localStorage.setItem("nombres", JSON.stringify(ultimosDonadores))
+            console.log(ultimosDonadores)
+            li.textContent = localStorage.getItem("nombres")
+            lista.appendChild(li)
+            $(".titMedia").text("Gracias Por Donar")
             const total = confirmar + 300;
             alert("su monto a pagar sera" + " " + total);
+            ultimosDonadores.unshift(datosForm.get("nombre"));
+            localStorage.setItem("nombres", JSON.stringify(ultimosDonadores));
+            console.log(ultimosDonadores);
+            li.textContent = localStorage.getItem("nombres");
+            lista.appendChild(li);
             $(".titMedia").text("Gracias Por Donar");
         }
         else {
+            alert("pago cancelado")
             alert("pago cancelado");
         }
 
+        event.preventDefault()
         event.preventDefault();
 
 
     })
-
-
-
     ScrollReveal().reveal('.logo', { delay: 500 });
     ScrollReveal().reveal('.secUno', { delay: 500 });
     ScrollReveal().reveal('.secDos', { delay: 500 });
